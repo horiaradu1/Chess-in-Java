a7599fb799967afd401f252cf4b9702837993d6e
k55592hr <horia.radu@student.manchester.ac.uk>
2020-02-07 11:04:04 +0000

    Game engine needed

--- chess/CheckInput.java
+++ chess/CheckInput.java
@@ -3,12 +3,6 @@ package chess;
 public class CheckInput {
 
 	public boolean checkCoordinateValidity(String input) {
-		if (input.length()!=2)
-			return false;
-		int a = Integer.parseInt(input.substring(0,1));
-		int b = (int) input.charAt(1);
-		if((a>=0 && a<=9) && (b>=97 && b<=104))
-			return true;
-		return false;
+		return input.matches("[0-7][a-h]");
 	}
 }
